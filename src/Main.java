2bece420b3f3c59a1ecdca51df797d332c31ec13
Lucas Martins <141669455+lucas2832@users.noreply.github.com>
2024-08-03 22:38:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
         public static void main(String[] args) {
         ReaderAndWriter reader = new ReaderAndWriter();
-        reader.reader("program.txt");
+        reader.reader("src/program.txt");
      }
 }
